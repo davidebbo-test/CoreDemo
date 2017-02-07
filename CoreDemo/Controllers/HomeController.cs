ef1da46a063568b78e81e8df0d986ffda121874b
David Ebbo <david.ebbo@gmail.com>
2017-02-06 21:12:53 -0800

    Use config

--- CoreDemo/Controllers/HomeController.cs
+++ CoreDemo/Controllers/HomeController.cs
@@ -4,11 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.ApplicationInsights;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.Extensions.Options;
 
 namespace CoreDemo.Controllers
 {
+    public class MyConfig
+    {
+        public string ApplicationName { get; set; }
+        public string Version { get; set; }
+    }
     public class HomeController : Controller
     {
+        IOptions<MyConfig> _config;
+
+        public HomeController(IOptions<MyConfig> config)
+        {
+            _config = config;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,7 +31,7 @@ namespace CoreDemo.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = $"Your application description page. {_config.Value.ApplicationName}!!";
             var telemetry = new TelemetryClient();
             telemetry.TrackEvent("Hello");
 
--- CoreDemo/Startup.cs
+++ CoreDemo/Startup.cs
@@ -27,6 +27,11 @@ namespace CoreDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add functionality to inject IOptions<T>
+            services.AddOptions();
+
+            // Add our Config object so it can be injected
+            services.Configure<Controllers.MyConfig>(Configuration);
             // Add framework services.
             services.AddMvc();
         }
